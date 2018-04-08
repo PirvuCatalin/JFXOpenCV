d9ec5072806a1c03f85bcbf975cd647b0ceb048b
catalin <cata_998@yahoo.com>
2018-04-08 15:19:36 +0300

    further cleaning of the code and extra integration

---
+++ src/sample/Controller.java
@@ -0,0 +1,411 @@
+package sample;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Core;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.videoio.VideoCapture;
+
+import Utils.Utils;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import com.openalpr.jni.Alpr;
+import com.openalpr.jni.AlprPlate;
+import com.openalpr.jni.AlprPlateResult;
+import com.openalpr.jni.AlprResults;
+
+/**
+ ** The controller for our application, where the application logic is
+ ** implemented. It handles the button for starting/stopping the camera and the
+ ** acquired video stream.
+ **/
+
+public class Controller
+{
+
+    // the FXML button
+    @FXML
+    private Button button;
+
+    // the FXML image view
+    @FXML
+    private ImageView currentFrame;
+
+    // a timer for acquiring the video stream
+    private ScheduledExecutorService timer;
+
+    // the OpenCV object that realizes the video capture
+    private VideoCapture capture = new VideoCapture();
+
+    // a flag to change the button behavior
+    private boolean cameraActive = false;
+
+    // the id of the camera to be used
+    private static int cameraId = 0;
+
+     /** The action triggered by pushing the button on the GUI
+     ** @param eventthe push button event
+     **/
+
+    // this is used to check if it's the time to scan for motion
+    private int currentMotionTime=0;
+
+    // Mat used to check for motion
+    private Mat frameDelta = new Mat();
+
+    private String ScanForCar()throws Exception {
+        // country: eu for Europe, us for USA
+        // configfile: the location of openalpr.conf
+        // runtimeDataDir: the location of runtime_data
+        // licensePlate: the location of the license plate
+        String country = "eu",
+                configfile = "openalpr.conf",
+                runtimeDataDir = "runtime_data",
+                licensePlate;
+
+        // detector initialization. do this once and don't mess with the files (parameters)
+        Alpr alpr = new Alpr(country, configfile, runtimeDataDir);
+
+        // set pattern to Romania (runtime_data/post_process/eu.patterns)
+        // a.k.a. searches first for this kind of plates to decrease detection time
+        alpr.setDefaultRegion("ro");
+
+        // set the number of plates to return PER EACH SCAN
+        // i.e. after each frame is scanned it returns a maximum of numberOfCandidates plates
+        int numberOfCandidates = 3;
+        alpr.setTopN(numberOfCandidates);
+
+        // this is the number of frames to scan
+        int numberOfFramesToDetect = 4;
+
+        // these strings+floats are used in order to keep the values after each scan for future processing
+        String[][] plateName = new String[numberOfFramesToDetect][numberOfCandidates];
+        float[][] plateConfidence = new float[numberOfFramesToDetect][numberOfCandidates];
+        // variable j used to populate plateName and plateConfidence vectors
+        int j;
+
+        // this is where i'll add individual processing times
+        float totalProcessingTime = 0;
+
+        // entering the loop that scans EACH frame
+        for(int currentFrameToDetect = 0; currentFrameToDetect < numberOfFramesToDetect; currentFrameToDetect++) {
+
+            // the location of the licensePlate
+            // !if you want this to work, use the location of your local license plate
+            // !!!If you want to use another test images, update the number of frame too!!!
+            licensePlate = "test_images\\test1 ("+(1+currentFrameToDetect)+").jpg";
+
+            // it does what is says;
+            // this is the main processing time eater
+            AlprResults results = alpr.recognize(licensePlate);
+
+            // Debug Only: to make the scan results look better
+            System.out.println("Plate Number:     Confidence:");
+
+            // The following for statement initializes "result" with the first AND ONLY object in the
+            // "results" list. If no object is present, this statement is skipped.
+            for (AlprPlateResult result : results.getPlates()) {
+                j = 0;
+
+                // The following for statement declares "plate" object and then passes
+                // each value in the "result" to it
+                for (AlprPlate plate : result.getTopNPlates()) {
+
+                    // there we store the name and confidence for each plate
+                    plateName[currentFrameToDetect][j] = plate.getCharacters();
+                    plateConfidence[currentFrameToDetect][j] = plate.getOverallConfidence();
+
+                    // Debug Only: printing current scan results
+                    System.out.println(
+                            plateName[currentFrameToDetect][j] +
+                                    "               " +
+                                    plateConfidence[currentFrameToDetect][j]
+                    );
+
+                    j++;
+                }
+            }
+
+            // add current frame processing time to total processing time
+            totalProcessingTime += results.getTotalProcessingTimeMs();
+        }
+
+        // prints total processing time
+        System.out.println(totalProcessingTime);
+
+        // we will use "x" in the rest of the code to mark the reference frame (see next comment)
+        // we will also use "j" as a variable
+        int x = 0;
+
+        // The reference frame is the frame that we will use to find the plate
+        // with the highest confidence rate. First, we will find a frame that
+        // has "numberOfCandidates" plates. If there aren't that many plates, it
+        // checks for the frame that has "numberOfCandidates-1" plates and so on.
+        // If we won't do this, there could be ignored plates and we don't want this
+        for(j = numberOfCandidates - 1; j >= 0; j--){
+            while(plateName[x][j] == null) x++;
+            if(x == numberOfCandidates && j!=0) {
+                x = 0;
+            }
+            else {
+                break;
+            }
+        }
+
+        //this is the maximum number of plates found in a frame
+        int actualNumberOfCandidates = j + 1;
+
+        // if this statement is true then there are no plates detected in any frame
+        if(x == numberOfCandidates && j == 0){
+            System.err.println("No plate detected in the frames");
+            System.exit(1);
+        }
+
+        // This is where i'll add the confidence rate found in all frames
+        // at the confidence rate of the reference frame
+        for(j = 0; j < numberOfCandidates; j++) {
+
+            // Testing that we're adding the confidence rate to the same plate
+            // found in the reference plate and also skipping to add the same
+            // confidence rate twice "x!=i"
+            for (int i = 0; i < numberOfFramesToDetect; i++){
+
+                for(int k = 0; k < numberOfCandidates; k++){
+                    if (plateName[i][k] != null)
+                    if (((plateName[i][k]).equals(plateName[x][j])) && x!=i ) {
+                        plateConfidence[x][j] += plateConfidence[i][k];
+                    }
+                }
+
+            }
+
+        }
+
+        // Finding the maximum confidence rate a.k.a. finding the actual plate number
+        // and the actual confidence rate (TOTAL NOT AVERAGE)
+        float maximumRate = plateConfidence[x][0];
+        int actualPlateNumber = 0;
+        for(int i = 1; i < actualNumberOfCandidates; i++){
+            if(plateConfidence[x][i] > maximumRate){
+                maximumRate = plateConfidence[x][i];
+                actualPlateNumber = i;
+            }
+        }
+
+        // Debug Only: printing plate number and the confidence rate
+        System.out.println("Total confidence rate(debug only):"+plateConfidence[x][actualPlateNumber]);
+        System.out.println("Plate number found:"+plateName[x][actualPlateNumber]);
+
+
+        // make sure to call this to release memory.
+        alpr.unload();
+        return plateName[x][actualPlateNumber];
+    }
+
+    @FXML
+    protected void startCamera (ActionEvent event)
+    {
+        // loads openalpr libraries
+        try {
+            System.loadLibrary("liblept170");
+            System.loadLibrary("opencv_world300");
+            System.loadLibrary("openalpr");
+        } catch (UnsatisfiedLinkError e) {
+            System.err.println("Native code library failed to load.\n" + e);
+            System.exit(1);
+        }
+        if (!this.cameraActive)
+        {
+            // start the video capture
+            this.capture.open(cameraId);
+
+            // is the video stream available?
+            if (this.capture.isOpened())
+            {
+                this.cameraActive = true;
+
+                // set this to schedule when to scan for motion
+                final int timeToMotionScan=15;
+
+                // takes the first frame after camera is opened
+                // to prevent errors
+                frameDelta = grabFrame();
+
+                // grab a frame every 33 ms (30 frames/sec)
+                Runnable frameGrabber = new Runnable(){
+                    @Override
+                    public void run()
+                    {
+                        // effectively grab and process a single frame
+                        Mat frame = grabFrame();
+
+                        // convert and show the frame
+                        Image imageToShow = Utils.mat2Image(frame);
+                        updateImageView(currentFrame, imageToShow);
+
+                        // Checks if it should scan for motion
+                        if(currentMotionTime++==timeToMotionScan){
+                            currentMotionTime=0;
+
+                            Mat firstFrame = frameDelta;
+                            List<MatOfPoint> contours = new ArrayList();
+
+                            // this is the minimum area to consider it a motion
+                            double maxArea = 50;
+
+                            Imgproc.GaussianBlur(firstFrame, firstFrame, new Size(3, 3), 0);
+                            Imgproc.GaussianBlur(frame, frame, new Size(3, 3), 0);
+
+                            Core.subtract(firstFrame, frame, frameDelta);
+
+                            Imgproc.cvtColor(frameDelta, frameDelta, Imgproc.COLOR_BGR2GRAY);
+
+                            // modify third parameter (thresh) to be more/less sensitive
+                            // if thresh is low -> the scanner is more sensible
+                            Imgproc.threshold(frameDelta, frameDelta, 50, 255, Imgproc.THRESH_BINARY);
+
+                            Mat v = new Mat();
+                            Mat vv = frameDelta.clone();
+
+                            Imgproc.findContours(vv, contours, v, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+
+                            for(int idx = 0; idx < contours.size(); idx++)
+                            {
+                                Mat contour = contours.get(idx);
+                                double contourarea = Imgproc.contourArea(contour);
+                                if (contourarea > maxArea){
+
+                                    // HERE IS WHAT HAPPENS IF MOVEMENT HAS BEEN FOUND
+                                    try {
+                                        String s = ScanForCar();
+                                        System.out.println("Plate number returned by the method: "+s);
+                                    }
+                                    catch (Exception e) {
+                                        System.err.print("Error at the entrance in the plate number scanner method " + e);
+                                    }
+                                    break;
+                                }
+                            }
+
+                            // takes a frame to compare on future scans
+                            frameDelta = grabFrame();
+                        }
+                    }
+                };
+
+                this.timer = Executors.newSingleThreadScheduledExecutor();
+
+                // modify third argument to increase/decrease the time to wait for grabbing a new frame
+                // 33 means 30FPS (Math!!!)
+                this.timer.scheduleAtFixedRate(frameGrabber, 0, 33, TimeUnit.MILLISECONDS);
+
+                // update the button content
+                this.button.setText("Stop Camera");
+            }
+            else
+            {
+                // log the error
+                System.err.println("Impossible to open the camera connection...");
+            }
+        }
+        else
+        {
+            // the camera is not active at this point
+            this.cameraActive = false;
+
+            // update again the button content
+            this.button.setText("Start Camera");
+
+            // stop the timer
+            this.stopAcquisition();
+        }
+    }
+    /** Get a frame from the opened video stream (if any)
+     ** @return the {@link Mat} to show
+     **/
+
+    private Mat grabFrame()
+    {
+        // init everything
+        Mat frame = new Mat();
+
+        // check if the capture is open
+        if (this.capture.isOpened())
+        {
+            try
+            {
+                // read the current frame
+                this.capture.read(frame);
+            }
+            catch (Exception e)
+            {
+                // log the error
+                System.err.println("Exception during the image elaboration: " + e);
+            }
+        }
+        return frame;
+    }
+
+    /**
+     ** Stop the acquisition from the camera and release all the resources
+     **/
+
+    private void stopAcquisition()
+    {
+        if (this.timer!=null && !this.timer.isShutdown())
+        {
+            try
+            {
+                // stop the timer
+                this.timer.shutdown();
+                this.timer.awaitTermination(33, TimeUnit.MILLISECONDS);
+            }
+            catch (InterruptedException e)
+            {
+                // log any exception
+                System.err.println("Exception in stopping the frame capture, trying to release the camera now... " + e);
+            }
+        }
+
+        if (this.capture.isOpened())
+        {
+            // release the camera
+            this.capture.release();
+        }
+    }
+
+    /**
+     * Update the {@link ImageView} in the JavaFX main thread
+     *
+     * @param view
+     *            the {@link ImageView} to update
+     * @param image
+     *            the {@link Image} to show
+     */
+
+    private void updateImageView(ImageView view, Image image)
+    {
+        Utils.onFXThread(view.imageProperty(), image);
+    }
+
+    /**
+     * On application close, stop the acquisition from the camera
+     */
+
+    protected void setClosed()
+    {
+        this.stopAcquisition();
+    }
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,64 @@
+package sample;
+
+import org.opencv.core.Core;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+/**
+ * The main class for a JavaFX application. It creates and handle the main
+ * window with its resources (style, graphics, etc.).
+ */
+
+public class Main extends Application
+{
+    @Override
+    public void start(Stage primaryStage)
+    {
+        try
+        {
+            // load the FXML resource
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+
+            // store the root element so that the controllers can use it
+            BorderPane rootElement = (BorderPane) loader.load();
+
+            // create and style a scene
+            Scene scene = new Scene(rootElement, 800, 600);
+
+            //scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            // create the stage with the given title and the previously created
+            // scene
+            primaryStage.setTitle("Camera Systems");
+            primaryStage.setScene(scene);
+
+            // show the GUI
+            primaryStage.show();
+
+            // set the proper behavior on closing the application
+            Controller controller = loader.getController();
+
+            primaryStage.setOnCloseRequest((we -> controller.setClosed()));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * For launching the application...
+     * @param args optional params
+     */
+
+    public static void main(String[] args)
+    {
+        // load the native OpenCV library
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        launch(args);
+    }
+}
